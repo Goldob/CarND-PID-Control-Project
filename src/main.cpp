0aa77fad58e9f2f7d5eba0877aadde5738ed3b42
Adam <gotlib.adam+dev@gmail.com>
2019-02-01 02:38:43 +0000

    Use PID to control the vehicle

--- src/main.cpp
+++ src/main.cpp
@@ -34,9 +34,8 @@ int main() {
   uWS::Hub h;
 
   PID pid;
-  /**
-   * TODO: Initialize the pid variable.
-   */
+  
+  pid.Init(0.25, 0.0, 1.5);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
@@ -56,13 +55,13 @@ int main() {
           double cte = std::stod(j[1]["cte"].get<string>());
           double speed = std::stod(j[1]["speed"].get<string>());
           double angle = std::stod(j[1]["steering_angle"].get<string>());
-          double steer_value;
-          /**
-           * TODO: Calculate steering value here, remember the steering value is
-           *   [-1, 1].
-           * NOTE: Feel free to play around with the throttle and speed.
-           *   Maybe use another PID controller to control the speed!
-           */
+          
+          std::cout << "  " << cte << ", " <<std::endl;
+          pid.UpdateError(cte);
+          
+          double steer_value = -pid.TotalError();
+          if (steer_value < -1.0) steer_value = -1.0;
+          if (steer_value >  1.0) steer_value =  1.0; 
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
